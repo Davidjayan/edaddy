23d865342e3d20c1f8862cedd7895af397c4ca4f
Davidjayan <davidjayandj@gmail.com>
2023-09-13 12:23:22 +0530

    [Davidjayan] Route added

--- src/routes/fileRoutes.ts
+++ src/routes/fileRoutes.ts
@@ -1,7 +1,7 @@
 import express from "express";
 import fileController from "../controllers/fileControllers";
 const router = express.Router();
-
+router.route('/').get((req,res)=>res.status(200).send('Ok'))
 router.route("/save-data").post(fileController.saveData);
 router.route('/get-file/:id').get(fileController.getFile);
 router.route('/list').get(fileController.list);
